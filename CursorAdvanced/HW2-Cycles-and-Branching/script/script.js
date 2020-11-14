2deecb003bc28f5b7908dc9144318cb604b21519
Volodymyr Reient <4reient4@gmail.com>
2020-11-14 14:18:54 +0200

    Add files via upload

---
+++ CursorAdvanced/HW2-Cycles-and-Branching/script/script.js
@@ -0,0 +1,22 @@
+let getN = +prompt("Enter the first integer number");
+const getM = +prompt("Enter the second integer number more than the first");
+const skipPairs = confirm("skip even numbers?");
+let sum = 0;
+
+if (Number.isInteger(getN) && Number.isInteger(getN) && getN < getM) {
+    if (skipPairs) {
+       while (getN <= getM) {
+        if(getN%2 === 1)  sum += getN;
+        getN++;
+       }
+       console.log(sum);       
+    } else {
+        while (getN <= getM) {
+        sum += getN;
+        getN++;
+        }
+        console.log(sum);
+    }
+} else {
+    console.log("Enter an integer");
+}
