050d3f098fd54e68211bf4e3228f23a6003b31a4
Volodymyr Reient <4reient4@gmail.com>
2020-11-14 15:52:05 +0200

    renamed variables

--- CursorAdvanced/HW2-Cycles-and-Branching/script/script.js
+++ CursorAdvanced/HW2-Cycles-and-Branching/script/script.js
@@ -1,22 +1,24 @@
-let getN = +prompt("Enter the first integer number");
-const getM = +prompt("Enter the second integer number more than the first");
+document.addEventListener("DOMContentLoaded", () => {
+let numberStart = +prompt("Enter the first integer number");
+const  numberEnd = +prompt("Enter the second integer number more than the first");
 const skipPairs = confirm("skip even numbers?");
 let sum = 0;
 
-if (Number.isInteger(getN) && Number.isInteger(getN) && getN < getM) {
+if (Number.isInteger(numberStart) && Number.isInteger(numberEnd) && numberStart <numberEnd) {
     if (skipPairs) {
-       while (getN <= getM) {
-        if(getN%2 === 1)  sum += getN;
-        getN++;
+       while (numberStart <= numberEnd) {
+        if(numberStart%2 === 1)  sum += numberStart;
+        numberStart++;
        }
-       console.log(sum);       
+       console.log(`Сума: ${sum}`);       
     } else {
-        while (getN <= getM) {
-        sum += getN;
-        getN++;
+        while (numberStart <= numberEnd) {
+        sum += numberStart;
+        numberStart++;
         }
-        console.log(sum);
+        console.log(`Сума: ${sum}`);
     }
 } else {
     console.log("Enter an integer");
 }
+});
