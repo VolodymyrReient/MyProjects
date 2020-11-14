73158bcd3bdb80c04d980f8512a68e9c31b83cd1
Volodymyr Reient <4reient4@gmail.com>
2020-11-14 15:53:43 +0200

    renamed variables

--- CursorAdvanced/HW2-Cycles-and-Branching/script/script.js
+++ CursorAdvanced/HW2-Cycles-and-Branching/script/script.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-let numberStart = +prompt("Enter the first integer number");
-const  numberEnd = +prompt("Enter the second integer number more than the first");
-const skipPairs = confirm("skip even numbers?");
-let sum = 0;
+    let numberStart = +prompt("Enter the first integer number");
+    const  numberEnd = +prompt("Enter the second integer number more than the first");
+    const skipPairs = confirm("skip even numbers?");
+    let sum = 0;
 
-if (Number.isInteger(numberStart) && Number.isInteger(numberEnd) && numberStart <numberEnd) {
-    if (skipPairs) {
-       while (numberStart <= numberEnd) {
-        if(numberStart%2 === 1)  sum += numberStart;
-        numberStart++;
-       }
-       console.log(`Сума: ${sum}`);       
-    } else {
+    if (Number.isInteger(numberStart) && Number.isInteger(numberEnd) && numberStart <numberEnd) {
+        if (skipPairs) {
         while (numberStart <= numberEnd) {
-        sum += numberStart;
-        numberStart++;
+            if(numberStart%2 === 1)  sum += numberStart;
+            numberStart++;
         }
-        console.log(`Сума: ${sum}`);
+        console.log(`Сума: ${sum}`);       
+        } else {
+            while (numberStart <= numberEnd) {
+            sum += numberStart;
+            numberStart++;
+            }
+            console.log(`Сума: ${sum}`);
+        }
+    } else {
+        console.log("Enter an integer");
     }
-} else {
-    console.log("Enter an integer");
-}
 });
