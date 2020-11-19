4cb1eb5e48bc8eebd0ab792c0653588b35081c9e
Volodymyr Reient <4reient4@gmail.com>
2020-11-19 10:38:05 +0200

    integer check, simplified code

--- CursorAdvanced/HW2-Cycles-and-Branching/script/script.js
+++ CursorAdvanced/HW2-Cycles-and-Branching/script/script.js
@@ -1,30 +1,23 @@
 let numberStart;
 do {
     numberStart = +prompt("Enter the first integer number", 0);
-} while (!isFinite(numberStart));
-
+} while (!isFinite(numberStart) || !Number.isInteger(numberStart));
 let  numberEnd;
 do {
      numberEnd = +prompt("Enter the second integer number more than the first", 0);
-} while (!isFinite(numberEnd));
+} while (!isFinite(numberEnd) || !Number.isInteger(numberEnd));
    
 const skipPairs = confirm("skip even numbers?");
 let sum = 0;
 
-if (Number.isInteger(numberStart) && Number.isInteger(numberEnd) && numberStart <numberEnd) {
-  if (skipPairs) {
-    while (numberStart <= numberEnd) {
-        if(numberStart%2 === 1)  sum += numberStart;
-         numberStart++;
-    }
-    console.log(`Сума: ${sum}`);       
+if (numberStart < numberEnd) {
+for (let i = numberStart; i <= numberEnd; i++) {
+if (skipPairs && i%2 ===0) {
+continue;
+}
+sum += i;
+}
+console.log(`Сума: ${sum}`);
 } else {
-     while (numberStart <= numberEnd) {
-    sum += numberStart;
-    numberStart++;
-    }
-    console.log(`Сума: ${sum}`);
-    }
-} else {
-     console.log("Enter an integer");
+     console.log("Enter a number more than the first");
 }
