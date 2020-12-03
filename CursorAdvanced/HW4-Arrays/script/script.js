b7fa2fe612ac8b09a3aa3a9a4a19b349a792e545
Volodymyr Reient <4reient4@gmail.com>
2020-12-03 10:38:59 +0200

    substitute for-map or forEach,addedarrow functions

--- CursorAdvanced/HW4-Arrays/script/script.js
+++ CursorAdvanced/HW4-Arrays/script/script.js
@@ -7,41 +7,39 @@ const studentsPairs = getPairs();
 const topicsPairs = appointmentTopics();
 
 function getPairs () {
-    const studentsPairs = [];
-        for (let i =0; i < male.length; i++) {
-            studentsPairs[i] = [male[i], female[i]].flat();
-    }
+    let studentsPairs = [];
+    male.map((value, index) => {
+        studentsPairs[index] = [value, female[index]];
+    });
     return studentsPairs;
 }
 console.log(studentsPairs);
 
 function appointmentTopics() {
-    const topics = []; 
-        for (let i = 0; i < 3; i++) {
-            topics[i] = studentsPairs[i].concat(themes[i]);
-  }
-  return topics;
+    let topics = []; 
+    studentsPairs.forEach((value, index) => {
+        topics[index] = [[value, themes[index]].flat()];
+    });
+  return topics.flat();
 }
 console.log(topicsPairs);
 
 function comparisonGrades() {
-    const evaluation = []; 
-        for (let i = 0; i < students.length; i++) {
-            evaluation[i] = [students[i], marks[i]];
-        }
+    let evaluation = []; 
+    students.map((value, index) => {
+        return evaluation[index] = [value, marks[index]];
+    }); 
   return evaluation;
 }
 console.log(comparisonGrades());
 
-function getRandomIntInclusive(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
+const getRandomIntInclusive = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+getRandomIntInclusive(2, 5);
 function groupAssessments() {
-    const groupReting = [];
-    for (let i = 0; i < studentsPairs.length; i++) {
-        groupReting[i] = [topicsPairs[i], getRandomIntInclusive(1, 5)].flat();
-    }
-    return groupReting;
+    let groupReting = [];
+    topicsPairs.map((value, index) => {
+        groupReting[index] = [[value, getRandomIntInclusive(2, 5)].flat()];
+    });
+    return groupReting.flat(    );
 }
 console.log(groupAssessments());
