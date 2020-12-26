f1cce0990e9609e52956f44938a293ff73e587cc
Volodymyr Reient <4reient4@gmail.com>
2020-12-26 22:32:23 +0200

    amendments made

--- CursorAdvanced/HW7-Locking-Keyword-this/script/script.js
+++ CursorAdvanced/HW7-Locking-Keyword-this/script/script.js
@@ -1,17 +1,21 @@
 const ukraine = { 
     tax: 0.195, 
     middleSalary: 1789, 
-    vacancies: 11476 
+    vacancies: 11476,
+    name: "Ukraine"
+    
 };
 const latvia = { 
     tax: 0.25, 
     middleSalary: 1586, 
-    vacancies: 3921 
+    vacancies: 3921,
+    name: "Latvia"
 };
 const litva = { 
     tax: 0.15, 
     middleSalary: 1509, 
-    vacancies: 1114 
+    vacancies: 1114,
+    name: "Litva" 
 };
 
 const getMyTaxes = function (salary) {
@@ -38,23 +42,45 @@ console.log(`Загальний податок в Україні: ${getTotalTaxe
 console.log(`Загальний податок в Латвії: ${getTotalTaxes.call(latvia)}`);
 console.log(`Загальний податок в Литві: ${getTotalTaxes.call(litva)}`);
 
-const getMySalary = function () {
-    const startSalary = 1500;
-    const endSalary = 2000;
-    let salary = Math.floor(Math.random() * (startSalary - endSalary + 1)) + endSalary;
-    let taxes = +(salary * this.tax).toFixed(2);
-    let profit = +(salary - taxes).toFixed(2);
-    const wage = {
-        salary: salary,
-        profit: profit,
-        taxes: taxes,
-    };
-    return  JSON.stringify(wage);
-}
+// const getMySalary = function () {
+//     const startSalary = 1500;
+//     const endSalary = 2000;
+//     let salary = Math.floor(Math.random() * (startSalary - endSalary + 1)) + endSalary;
+//     let taxes = +(salary * this.tax).toFixed(2);
+//     let profit = +(salary - taxes).toFixed(2);
+//     const wage = {
+//         salary: salary,
+//         profit: profit,
+//         taxes: taxes,
+//     };
+//     return  JSON.stringify(wage);
+// }
 
-setInterval( function(){
-    console.log(`Загальна інформація про зарплати в Україні: ${getMySalary.call(ukraine)}`);
-    console.log(`Загальна інформація про зарплати в Латвії: ${getMySalary.call(latvia)}`);
-    console.log(`Загальна інформація про зарплати в Литві: ${getMySalary.call(litva)}`)
-    }, 10000 );
+
+const getMySalary = (country) => {
+    setInterval(() => {
+        const startSalary = 1500;
+        const endSalary = 2000;
+        const salary = Math.floor(Math.random() * (startSalary - endSalary + 1)) + endSalary;
+        const taxes = +(salary * country.tax).toFixed(2);
+        let profit = +(salary - taxes).toFixed(2);
+        const wage = {
+            salary: salary,
+            profit: profit,
+            taxes: taxes
+        };
+        console.log( `Загальна інформація про зарплати в ${country.name} ${JSON.stringify(wage)}`);
+    }, 10000)
+    }
+
+    getMySalary(ukraine);
+    getMySalary(latvia);
+    getMySalary(litva);
+
+
+// setInterval( function(){
+    // console.log(`Загальна інформація про зарплати в Україні: ${getMySalary(ukraine)}`);
+    // console.log(`Загальна інформація про зарплати в Латвії: ${getMySalary.call(latvia)}`);
+    // console.log(`Загальна інформація про зарплати в Литві: ${getMySalary.call(litva)}`)
+    // }, 10000 );
 
