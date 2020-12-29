687025c527d2ffe865209766fbb9954ecc0a2026
Volodymyr Reient <4reient4@gmail.com>
2020-12-30 01:07:06 +0200

    Add files via upload

---
+++ CursorAdvanced/HW14-Webpack/src/index.js
@@ -0,0 +1,10 @@
+import {getMaxDigit} from "./modules/02";
+import {getRandomArray} from "./modules/05";
+import css from "./style/09.css";
+import img from './img/bg.jpeg';
+import {documentAll} from "./modules/09";
+documentAll();
+console.log(getMaxDigit(123456));
+console.log(getRandomArray(10, 3, 50));
+
+console.log("Hello!");
---
+++ CursorAdvanced/HW14-Webpack/src/modules/09.js
@@ -0,0 +1,32 @@
+ function documentAll() {
+const squareAll = document.querySelector(".wrapper");
+const square = document.querySelectorAll(".square");
+const btn = document.querySelector(".btn");
+let timer; 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+ let randomColor = setInterval(function() {
+  square.forEach((e) => {
+        e.style.backgroundColor = 'rgb('+getRandomInt(0,255)+', '+getRandomInt(0,255)+', '+getRandomInt(0,255)+')'; 
+      });
+}, 1000);
+function generateBlocks() {
+    squareAll.classList.toggle("wrapper-show");   
+    // if(!randomColor.classList.contains("wrapper-show")) {
+    //   clearInterval(randomColor);
+    // }  
+}
+btn.addEventListener("click", generateBlocks);
+}
+
+
+export {documentAll};
+
+
+
+
+
+
+
+
