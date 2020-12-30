147996f0adf2ea1ff76a5bbb813a2009387bf7a8
Volodymyr Reient <4reient4@gmail.com>
2020-12-30 16:43:42 +0200

    Update script.js

--- CursorAdvanced/HW9-DOM/script/script.js
+++ CursorAdvanced/HW9-DOM/script/script.js
@@ -5,15 +5,13 @@ let timer;
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
- let randomColor = setInterval(function() {
+ const randomColor = setInterval(function() {
   square.forEach((e) => {
         e.style.backgroundColor = 'rgb('+getRandomInt(0,255)+', '+getRandomInt(0,255)+', '+getRandomInt(0,255)+')'; 
       });
 }, 1000);
 function generateBlocks() {
     squareAll.classList.toggle("wrapper-show");   
-    if(squareAll.classList.contains("wrapper-show")) {
-      clearInterval(randomColor);
     }  
 }
 btn.addEventListener("click", generateBlocks);
